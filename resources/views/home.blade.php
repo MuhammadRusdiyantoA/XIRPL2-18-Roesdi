563a7e97d121c33dfe27c4ddb976c3f5f4e3f4ff
MuhammadRusdiyantoA <3103120152@student.smktelkom-pwt.sch.id>
2022-01-24 09:08:41 +0700

    First Commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/home-style.css">
+    <title>Perpustakaan | Home</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <h1>Perpustakaan</h1>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="gallery">Gallery</a></li>
+                <li><a href="about">About</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <h1>Halaman Home</h1>
+    </main>
+
+<script src="js/home-script.js"></script>
+</body>
+</html>
